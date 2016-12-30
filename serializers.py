2e75905a5e19f1a779d200240115895a82ac4348
Arnol229 <bo.arnold@moovel.com>
2016-12-30 14:35:00 -0600

    fixed

--- serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from threat import IPDetails
-
-class DetailsSerializer(serializers.Serializer):
-    is_valid = serializers.Field()
-
-    # TODO: serialize the rest of your values in IPDetails here
-    # use of the simple serializers.Field() is acceptable
